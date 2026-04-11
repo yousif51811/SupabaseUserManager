d0e7ca5a3a9f3a347a83b0730dc4d09c2b0e37db
yousif51811 <yousif51811@gmail.com>
2026-04-11 08:45:04 +0400

    feat: Added views

---
+++ Services/SupabaseHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Supabase;
+namespace SupabaseUserManager.Services
+{
+    class SupabaseHandler
+    {
+    }
+}
---
+++ Views/AddUserWindowView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SupabaseUserManager.Views
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindowView.xaml
+    /// </summary>
+    public partial class AddUserWindowView : Window
+    {
+        public AddUserWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
